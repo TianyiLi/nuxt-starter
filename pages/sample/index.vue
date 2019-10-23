936cec5a5cd97d2fbbc40650a5a9a8d5cc6111c3
Paul Lee <e0991100238@gmail.com>
2019-10-23 16:08:31 +0800

    init project with typescript, pug, tailwindcss, stylus

---
+++ pages/sample/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class SampleMain extends Vue {
+  message = 'test'
+}
+</script>
+<template lang="pug">
+  .w-100.bg-black.text-white.active
+    | Hello world! {{message}}
+</template>
+<style lang="stylus" scoped>
+.active
+  height 20vh
+</style>
